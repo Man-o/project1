d024d9cb99bc794f434890613f84b2eaeda22d2f
Man-o <manoayyappan2503@gmail.com>
2024-08-15 08:20:37 +0530

    first-commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def longBoat(request):
+    return HttpResponse('This pub is best for drink')
+
